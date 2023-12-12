b8f8baf88696551e3386b58f21ff9b24859ee958
Oliver-Mutuku <olivermulei001@gmail.com>
2023-12-12 23:46:42 +0300

    implemented user authentication

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from email_validator import validate_email, EmailNotValidError
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 
@@ -8,6 +10,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '4a95a87d5b0f3af5855f9cc161de54f2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)    # creating a database instance
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes  
 '''even though we fixed the problem with the messy imports by creating a package, we still have to watch for circular 
